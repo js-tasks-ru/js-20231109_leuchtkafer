26b3604703e694f1a1d2c3766eedeed5c215fc50
m.chernova <m.chernova@ntechlab.com>
2023-11-26 23:42:59 +0300

    04-oop-basic-intro-to-dom: фикс по ревью

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,26 +1,22 @@
 export default class ColumnChart {
   chartHeight = 50;
-  link = '#';
   element;
 
-  constructor({ data, label, link, value, formatHeading} = {}) {
-    this.data = data ?? [];
-    this.label = label ?? '';
-    this.link = link ?? '';
-    this.value = value ? value.toLocaleString() : 0;
+  constructor({ data = [], label = '', link = '#', value = 0, formatHeading} = {}) {
+    this.data = data;
+    this.label = label;
+    this.link = link;
+    this.value = value;
+    this.formatHeading = formatHeading;
 
-    if (formatHeading) {
-      this.value = formatHeading(this.value);
-    }
-
-    this.render();
+    this.createElement();
   }
 
   columnClasses() {
-    return `column-chart ${!this.data.length && 'column-chart_loading'}`;
+    return `column-chart ${!this.data.length ? 'column-chart_loading' : ''}`;
   }
 
-  getColumns() {
+  createColumnsTemplate() {
     const maxValue = Math.max(...this.data);
 
     return this.data
@@ -37,18 +33,18 @@ export default class ColumnChart {
     return `<div class="${this.columnClasses()}" style="--chart-height: ${this.chartHeight}">
       <div class="column-chart__title">
         ${this.label}
-        ${this.link && `<a class="column-chart__link" href="${this.link}">View all</a>`}
+        ${this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : ''}
       </div>
       <div class="column-chart__container">
-        <div data-element="header" class="column-chart__header">${this.value}</div>
+        <div data-element="header" class="column-chart__header">${this.formatHeading ? this.formatHeading(this.value) : this.value}</div>
         <div data-element="body" class="column-chart__chart">
-           ${this.getColumns()}
+           ${this.createColumnsTemplate()}
         </div>
       </div>
     </div>`;
   }
 
-  render() {
+  createElement() {
     const element = document.createElement('template');
     element.insertAdjacentHTML('afterbegin', this.template);
     this.element = element.firstElementChild;
@@ -56,6 +52,7 @@ export default class ColumnChart {
 
   update(newData) {
     this.data = newData;
+    this.element.querySelector('.column-chart__chart').innerHTML = this.createColumnsTemplate();
   }
 
   destroy() {
