d17494fc1a3e862b0432f097a46aaa9d4a3cca81
m.chernova <m.chernova@ntechlab.com>
2023-12-02 15:49:34 +0300

    06-events-practice: 1-sortable-table-v2 - решение

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,153 @@
 export default class SortableTable {
+  element;
+  subElements = null;
+  previousSortedHeader;
+
   constructor(headersConfig, {
     data = [],
-    sorted = {}
+    sorted = {},
+    isSortLocally = true
   } = {}) {
+    this.header = headersConfig;
+    this.body = data;
+    this.sorted = sorted;
+    this.isSortLocally = isSortLocally;
 
+    this.createElement();
+
+    this.subElements = { body: this.element.querySelector('.sortable-table__body'), header: this.element.querySelector('.sortable-table__header') };
+
+    this.subElements.header.addEventListener('click', (event) => this.onHeaderClick(event));
+
+    document.addEventListener("DOMContentLoaded", () => this.onDomLoaded());
+  }
+
+  createHeaderTemplate() {
+    return this.header
+        .map(item => {
+          return `<div class="sortable-table__cell" data-id="${item.id}" data-sortable="${item.sortable}">
+        <span>${item.title}</span>
+        ${item.sortable ? `<span data-element="arrow" class="sortable-table__sort-arrow">
+          <span class="sort-arrow"></span>
+        </span>` : ''}
+      </div>`;
+        })
+        .join('');
+  }
+
+  createCellsTemplate(item) {
+    return this.header
+        .map(title => {
+          if (title.id === 'images') {
+            return `<img class="sortable-table-image" alt="Image" src=${item[title.id][0].url}>`;
+          }
+          return `<div class="sortable-table__cell">${item[title.id]}</div>`;
+        }).join('');
+
+  }
+
+  createBodyTemplate() {
+    return this.body
+        .map(item => {
+          return `<a href="/products/${item.id}" class="sortable-table__row">
+            ${this.createCellsTemplate(item)}
+          </a>`;
+        })
+        .join('');
+  }
+
+  get template() {
+    return `<div class="sortable-table">
+      <div data-element="header" class="sortable-table__header sortable-table__row">
+          ${this.createHeaderTemplate()}
+      </div>
+      
+      <div data-element="body" class="sortable-table__body">
+          ${this.createBodyTemplate()}
+      </div>
+    </div>`;
+  }
+
+  createElement() {
+    const element = document.createElement('template');
+    element.insertAdjacentHTML('afterbegin', this.template);
+    this.element = element.firstElementChild;
+  }
+
+  sortStrings(field, order) {
+    if (order === 'desc') {
+      return this.body.sort((a, b) => b[field].localeCompare(a[field], ['ru', 'en'], { caseFirst: 'upper' }));
+    }
+
+    return this.body.sort((a, b) => a[field].localeCompare(b[field], ['ru', 'en'], { caseFirst: 'upper' }));
+  }
+
+  sortNumbers(field, order) {
+    if (order === 'desc') {
+      return this.body.sort((a, b) => b[field] - a[field]);
+    }
+
+    return this.body.sort((a, b) => a[field] - b[field]);
+  }
+
+  sort(field, order) {
+    if (this.previousSortedHeader) {
+      order = this.previousSortedHeader.getAttribute('data-order') === 'asc' ? 'desc' : 'asc';
+      this.previousSortedHeader.removeAttribute('data-order');
+    }
+
+    if (this.isSortLocally) {
+      this.sortOnClient(field, order);
+    } else {
+      this.sortOnServer(field, order);
+    }
+
+    const targetTitle = document.querySelector(`[data-id="${field}"]`);
+
+    targetTitle.setAttribute('data-order', order);
+
+    this.previousSortedHeader = targetTitle;
+
+    this.subElements.body.innerHTML = this.createBodyTemplate();
+  }
+
+  sortOnClient(field, order) {
+    const currentSortType = this.header.find(item => item.id === field).sortType;
+
+    if (currentSortType === 'string') {
+      this.body = this.sortStrings(field, order);
+    } else {
+      this.body = this.sortNumbers(field, order);
+    }
+  }
+
+  sortOnServer(field, order) {
+    console.log(`Сортировка на сервере. Поле: ${field} порядок: ${order}`);
+  }
+
+  onHeaderClick(event) {
+    const targetDataset = event.target.dataset.hasOwnProperty('id') ? event.target.dataset : event.target.parentNode.dataset;
+    const { id, order, sortable } = targetDataset;
+
+    if (sortable) {
+      this.sort(id, order);
+    }
+  }
+
+  onDomLoaded() {
+    if (this.sorted) {
+      const { id, order } = this.sorted;
+      this.sort(id, order);
+    }
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.subElements.header.removeEventListener('click', (event) => this.onHeaderClick(event));
+    document.removeEventListener("DOMContentLoaded", () => this.onDomLoaded());
+    this.remove();
   }
 }
