0ab2bb65fbc1481cc21237715ec0f2723a4976d7
m.chernova <m.chernova@ntechlab.com>
2023-12-07 00:44:28 +0300

    07-async-code-fetch-api-part-1: 1-column-chart - решение

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -1,7 +1,56 @@
 import fetchJson from './utils/fetch-json.js';
+import ColumnChartV1 from "../../04-oop-basic-intro-to-dom/1-column-chart";
 
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ColumnChart {
+export default class ColumnChart extends ColumnChartV1 {
+  subElements;
+  element;
 
+  constructor({ url = '', label = '', range = {
+    from: '',
+    to: ''
+  }} = {}) {
+    super();
+
+    this.url = url;
+    this.label = label;
+    this.from = range.from;
+    this.to = range.to;
+
+    this.element = this.createElement();
+    this.createListener();
+    this.subElements = {
+      body: this.element.querySelector('div[data-element="body"]')
+    };
+  }
+  // специально не использовала второй аргумент params и положила параметры в ссылку, тк почему-то перестало работать в процессе решения задачи (параметры не прокидывались)
+  async loadData(from, to) {
+    this.element.classList.add('column-chart_loading');
+    return await fetchJson(`${BACKEND_URL}/${this.url}?from=${from}&to=${to}`);
+  }
+
+  convertDate(date) {
+    return date.toLocaleDateString('ru').split('.').reverse().join('-');
+  }
+
+  async update(from, to) {
+    let response;
+
+    try {
+      response = await this.loadData(this.convertDate(from), this.convertDate(to));
+      const values = Object.values(response);
+      const valuesLength = Object.values(response).length;
+      this.value = valuesLength;
+      super.update(values);
+      this.element.classList.remove('column-chart_loading');
+    } catch (error) {
+      new Error(error);
+    }
+    return response;
+  }
+
+  createListener() {
+    document.addEventListener('DOMContentLoaded', () => this.update(this.from, this.to));
+  }
 }
