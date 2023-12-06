30cc6d5d7a92f69dc7ee58a0ce147e3c7027a4e7
m.chernova <m.chernova@ntechlab.com>
2023-12-06 13:51:12 +0300

    06-events-practice: 3-double-slider - 1 итерация

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -1,3 +1,167 @@
 export default class DoubleSlider {
+  element;
+  thumbPositionLeft;
+  thumbPositionRight;
+  elementRect;
+  range;
+  progress;
 
+  constructor({
+    min = 50,
+    max = 150,
+    formatValue,
+    selected = {
+      from: 0,
+      to: 0
+    }
+  } = {}) {
+    this.min = min;
+    this.max = max;
+    this.formatValue = formatValue;
+    this.selected = {
+      from: selected.from,
+      to: selected.to,
+    };
+
+    this.createElement();
+    this.createListeners();
+    this.bar = this.element.querySelector('.range-slider__inner');
+    this.elementRect = this.element.getBoundingClientRect();
+    this.progress = this.element.querySelector('.range-slider__progress');
+    this.range = this.max - this.min;
+  }
+
+  get template() {
+    return `<div class="range-slider">
+      <span data-element="from">${this.formatValue ? this.formatValue(this.selected.from || this.min) : this.selected.from || this.min}</span>
+        <div class="range-slider__inner">
+          <span class="range-slider__progress" style="left: ${this.selected.from ?? this.min}%; right: ${this.selected.to ?? this.max}%"></span>
+          <span class="range-slider__thumb-left" style="left: ${this.selected.from}"></span>
+          <span class="range-slider__thumb-right" style="right: ${this.selected.to}"></span>
+        </div>
+      <span data-element="to">${this.formatValue ? this.formatValue(this.selected.to || this.max) : this.selected.to || this.max}</span>
+    </div>`;
+  }
+
+  createElement() {
+    const element = document.createElement('template');
+    element.insertAdjacentHTML('afterbegin', this.template);
+    this.element = element.firstElementChild;
+  }
+
+  moveAtLeft = (clientX) => {
+    const elem = this.element.querySelector('.range-slider__thumb-left');
+    const elemFrom = this.element.querySelector('[data-element="from"]');
+    const shiftX = clientX - this.thumbPositionLeft;
+    let valInPx = shiftX < 0 ? 0 : shiftX > this.elementRect.width ? this.max : shiftX;
+    if (shiftX < this.min) {
+      elemFrom.textContent = this.min;
+    }
+    // TODO: здесь логика чтобы левый ползунов не наползал на правый, но не падают тесты
+    // if (this.selected.to) {
+    //   const elem = this.element.querySelector('.range-slider__thumb-right');
+    //   valInPx = shiftX >= elem.offsetLeft ? elem.offsetLeft : shiftX;
+    // }
+    const valInPercent = valInPx / this.elementRect.width * 100;
+    elem.style.left = `${valInPercent}%`;
+    this.progress.style.left = `${valInPercent}%`;
+    const roundValue = (this.min + (this.range / 100 * valInPercent)).toFixed();
+    elemFrom.textContent = this.formatValue ? this.formatValue(roundValue) : roundValue;
+    this.selected.from = +roundValue;
+  }
+
+  moveAtRight = (clientX) => {
+    const elem = this.element.querySelector('.range-slider__thumb-right');
+    const elemTo = this.element.querySelector('[data-element="to"]');
+    const shiftX = this.thumbPositionRight - clientX;
+    let valInPx = shiftX < 0 ? 0 : shiftX > this.elementRect.width ? this.max : shiftX;
+
+    if (clientX > this.max) {
+      elemTo.textContent = this.max;
+    }
+    // TODO: здесь логика чтобы правый ползунок не наползал на левый, но падают тесты
+    // if (this.selected.from) {
+    //   const elem = this.element.querySelector('.range-slider__thumb-left');
+    //   valInPx = shiftX > elem.offsetLeft ? elem.offsetLeft : shiftX;
+    // }
+    const valInPercent = valInPx / this.elementRect.width * 100;
+    elem.style.right = `${valInPercent}%`;
+    this.progress.style.right = `${valInPercent}%`;
+    const roundValue = (this.max - (this.range / 100 * valInPercent)).toFixed();
+    elemTo.textContent = this.formatValue ? this.formatValue(roundValue) : roundValue;
+    this.selected.to = +roundValue;
+    if (clientX >= this.max) {
+      elemTo.textContent = this.max;
+    }
+  }
+
+  onDocumentPointermoveLeft = (event) => {
+    this.moveAtLeft(event.clientX);
+    this.dispatchEvent();
+  }
+
+  onDocumentPointermoveRight = (event) => {
+    this.moveAtRight(event.clientX);
+    this.dispatchEvent();
+  }
+
+  onDomLoaded = () => {
+    this.elementRect = this.element.getBoundingClientRect();
+  }
+
+  dispatchEvent = () => {
+    this.element.dispatchEvent(new CustomEvent('range-select', {
+      bubles: true,
+      detail: this.selected,
+    }));
+  }
+
+  onThumbLeftPointerdown = (event) => {
+    event.target.style.zIndex = '1000';
+    this.thumbPositionLeft = this.bar.getBoundingClientRect().left;
+
+    this.moveAtLeft(event.clientX);
+
+    document.addEventListener('pointermove', this.onDocumentPointermoveLeft);
+    document.addEventListener('pointerup', () => {
+      this.dispatchEvent();
+      document.removeEventListener('pointermove', this.onDocumentPointermoveLeft);
+    });
+  }
+
+  onThumbRightPointerdown = (event) => {
+    event.target.style.zIndex = '1000';
+    this.thumbPositionRight = this.bar.getBoundingClientRect().right;
+
+    this.moveAtRight(event.clientX);
+
+    document.addEventListener('pointermove', this.onDocumentPointermoveRight);
+    document.addEventListener('pointerup', () => {
+      this.dispatchEvent();
+      document.removeEventListener('pointermove', this.onDocumentPointermoveRight);
+    });
+  }
+
+  createListeners() {
+    this.element.querySelector('.range-slider__thumb-left').addEventListener('pointerdown', this.onThumbLeftPointerdown);
+    this.element.querySelector('.range-slider__thumb-right').addEventListener('pointerdown', this.onThumbRightPointerdown);
+    document.addEventListener('range-select', this.onThumbLeftPointerdown);
+    document.addEventListener('DOMContentLoaded', this.onDomLoaded);
+  }
+
+  destroyListeners() {
+    this.element.querySelector('.range-slider__thumb-left').removeEventListener('pointerdown', this.onThumbLeftPointerdown);
+    this.element.querySelector('.range-slider__thumb-right').removeEventListener('pointerdown', this.onThumbRightPointerdown);
+    document.removeEventListener('range-select', this.dispatchEvent);
+    document.removeEventListener('DOMContentLoaded', this.onDomLoaded);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.destroyListeners();
+    this.remove();
+  }
 }
